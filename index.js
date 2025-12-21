a5fb161f619daa8c83913c3d2691f4656b08e86c
kirigaya211 <kirigayarz@gmail.com>
2025-12-21 14:59:45 +0800

    Feature: restructure server routes and enhance template rendering

--- index.js
+++ index.js
@@ -1,21 +1,56 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const replaceTemplate =require('./modules/replaceTemplate')
 
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
-  if (pathName === "/" || pathName === "/overview") {
-    res.end("This is the overview");
-  } else if (pathName === "/product") {
-    res.end("This is the Product");
-  } else if (pathName === "/api") {
+  const { query, pathname } = url.parse(req.url, true);
+
+  //overview
+  if (pathname === "/" || pathname === "/overview") {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    const output = tempOverview.replace("{%Product_Cards%}", cardsHtml);
+    res.end(output);
+
+    //product
+  } else if (pathname === "/product") {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+    const product = dataObj[query.id]
+    const output = replaceTemplate(tempProduct,product);
+    res.end(output);
+
+    //api
+  } else if (pathname === "/api") {
     res.writeHead(200, {
       "Content-type": "application/json",
     });
-    res.end(data );
+    res.end(data);
+
+    //not found
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
