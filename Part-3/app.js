1e3e19c9083390a6ef0b350c025c179ab6017b5d
kirigaya211 <kirigayarz@gmail.com>
2026-01-04 23:07:50 +0800

    feat: restructure app.js to include middleware and improve route handling

--- Part-3/app.js
+++ Part-3/app.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 const port = 3000;
 
-const getTour = (req, res) => {
+app.use(express.json());
+app.use(morgan('dev'));
+
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     data: {},
   });
 };
 
-app.get('/',getTour);
+const getTour = (req,res)=>{
+    const id = req.params.id;
+    console.log(id);
+    res.status(200).json({
+        status:'success',
+        data:{}
+    });
+}
+
+
+
+app.get('/',getAllTours);
+app.get('/:id',getTour);
 
 app.listen(port, () => {
   console.log(`App is listening to ${port}`);
